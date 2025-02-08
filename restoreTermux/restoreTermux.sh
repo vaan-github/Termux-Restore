045dd12523c7cb8595307f5197bc016a10d55f81
Aniket Varma <vaan.origin@gmail.com>
2025-02-08 11:41:33 +0000

    fix issue and made code readable

--- restoreTermux/restoreTermux.sh
+++ restoreTermux/restoreTermux.sh
@@ -1,16 +1,40 @@
      ####### RESTORE COMMAND ########
 
-termux-change-repo && pkg install root-repo && pkg install x11-repo && apt update -y && apt upgrade -y && apt dist-upgrade -y && apt --fix-broken install -y && apt autoremove -y && apt autoclean && pkg install python -y && pip install virtualenv && python -m venv ~/venv && source ~/venv/bin/activate && python -m pip install --upgrade pip && pkg install nodejs && cd ~/ && git clone https://github.com/Ja7ad/W3Schools 
-&& cd ~/ && mkdir shortcuts && cd shortcuts && echo 'clear && cd ~/ && cd W3Schools termux-open-url http://localhost:8080 && python -m http.server 8080 && clear && exit 0 ' > w3school.sh && chmod +x w3school.sh
-&& cd ~/ && cd .. && cd usr/etc/profile.d && echo '#!/bin/bash
+termux-change-repo && \
+pkg install root-repo -y && \
+pkg install x11-repo -y && \
+apt update -y && \
+apt upgrade -y && \
+apt dist-upgrade -y && \
+apt --fix-broken install -y && \
+apt autoremove -y && \
+apt autoclean && \
+pkg install python -y && \
+pip install virtualenv && \
+python -m venv ~/venv && \
+source ~/venv/bin/activate && \
+python -m pip install --upgrade pip && \
+pkg install nodejs -y && \
+apt install git curl wget -y && \
+cd ~/ && \
+git clone https://github.com/Ja7ad/W3Schools && \
+mkdir -p ~/shortcuts && \
+cd ~/shortcuts && \
+echo 'clear && cd ~/W3Schools && termux-open-url http://localhost:8080 && python -m http.server 8080 && clear && exit 0' > w3school.sh && \
+chmod +x w3school.sh && \
+cd ~/ && \
+cd ../usr/etc/profile.d && \
+echo '#!/bin/bash
 
 ## ALIASES
-
 alias activate="source ~/venv/bin/activate"
 alias update="apt update && apt upgrade -y && apt dist-upgrade -y && apt --fix-broken install && apt autoremove -y && apt autoclean"
-alias w3s="clear && cd ~/ && cd W3Schools termux-open-url http://localhost:8080 && python -m http.server 8080 && clear && exit 0"
-alias aFile="cd ~/ && cd .. && cd usr/etc/profile.d && nano startup.sh && clear && echo '"'"'Alias File Closed'"'"'"
+alias w3s="clear && cd ~/W3Schools && termux-open-url http://localhost:8080 && python -m http.server 8080 && clear && exit 0"
+alias aFile="cd ~/../usr/etc/profile.d && nano startup.sh && clear && echo '\''Alias File Closed'\''"
 
 ## IN CONSOLE
 echo "python: (activate), W3School: (w3s), full-update: (update), Aliases:(aFile)"
-' > startup.sh && chmod +x startup.sh && bash -c "$(curl -fsSL https://git.io/Jei6P)" && exit && exit 0
+' > startup.sh && \
+chmod +x startup.sh && \
+bash -c "$(curl -fsSL https://git.io/Jei6P)" && \
+cd ~/ 
